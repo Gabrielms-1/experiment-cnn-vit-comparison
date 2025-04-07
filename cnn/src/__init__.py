a42fd9d694ead732bff6dbc6e1913a7320b106ec
gabrielms-1 <gabriel.sousa@syngenta.com>
2025-04-07 19:19:57 -0300

    adding resnet50 to the experiment

---
+++ cnn/src/__init__.py
@@ -0,0 +1,2 @@
+from resnet50 import ResNet50
+from dataset import FolderBasedDataset, create_dataloader
---
+++ cnn/src/resnet50.py
@@ -0,0 +1,93 @@
+import torch
+import torch.nn as nn
+
+
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, in_channels, mid_channels, stride=1, downsample=None):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels, mid_channels, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(mid_channels)
+
+        self.conv2 = nn.Conv2d(mid_channels, mid_channels, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(mid_channels)
+
+        self.conv3 = nn.Conv2d(mid_channels, mid_channels * self.expansion, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(mid_channels * self.expansion)
+
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+
+    def forward(self, x):
+        identity = x
+
+        out = self.relu(self.bn1(self.conv1(x)))
+        out = self.relu(self.bn2(self.conv2(out)))
+        out = self.bn3(self.conv3(out))
+
+        if self.downsample:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+        
+        return out
+
+
+class ResNet50(nn.Module):
+    def __init__(self, num_classes=5):
+        super().__init__()
+        self.in_channels = 64
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        self.layer1 = self._make_layer(64,  3)
+        self.layer2 = self._make_layer(128, 4, stride=2)
+        self.layer3 = self._make_layer(256, 6, stride=2)
+        self.layer4 = self._make_layer(512, 3, stride=2)
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512 * Bottleneck.expansion, num_classes)
+
+    def _make_layer(self, mid_channels, blocks, stride=1):
+        downsample = None
+        out_channels = mid_channels * Bottleneck.expansion
+
+        if stride != 1 or self.in_channels != out_channels:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_channels),
+            )
+
+        layers = [Bottleneck(self.in_channels, mid_channels, stride, downsample)]
+        self.in_channels = out_channels
+        for _ in range(1, blocks):
+            layers.append(Bottleneck(self.in_channels, mid_channels))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.relu(self.bn1(self.conv1(x)))   # Conv1
+        x = self.maxpool(x)                      # MaxPool
+
+        x = self.layer1(x)                       # Conv2_x
+        x = self.layer2(x)                       # Conv3_x
+        x = self.layer3(x)                       # Conv4_x
+        x = self.layer4(x)                       # Conv5_x
+
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+        
+        return x
+
+
+if __name__ == "__main__":
+    model = ResNet50(num_classes=5)
+    x = torch.randn(1, 3, 640, 640)
+    y = model(x)
+    print(y.shape)  
